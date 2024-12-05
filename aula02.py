198790c201a22672539d07ae1b55a502ff12a103
Alessandro Jesus <alessandrojesusdossantosjunior@gmail.com>
2024-12-05 09:46:37 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,11 @@
+print("Ola vei")
+nome_usuario = "Alex" #Nome - str - string
+idade_usuario = 19 #int - integer
+altura_usuario = 1.83 #float - ponto flutuante
+maior_idade = True #Booleano
+
+print("Seja bem vindo", nome_usuario)
+print("Sua idade é", idade_usuario "anos")
+print("Você tem",altura_usuario, "de altura", )
+print("Você é maior de idade?", maior_idade)
+
